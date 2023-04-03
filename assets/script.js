58f85439a56ff66495c7db45342cd0154210c751
Blake H <blake.hollingsworth80@gmail.com>
2023-04-03 13:39:27 -0600

    added more comments

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
 // On Website-Load we need to check for location and load local news articles
+//      // convert Geo-Location to an actual usable location for newscatcher filtering
+//      // save Geo-Location permissions to local storage
+
 // we need to set up a function for Jquery to add the news articles as a list to the page
 
 // We need to have an event listeners for a button that will reload the news articles based on seach criteria typed by the user
-// Criteria includes --- Title/keywords, Date, location, news source
-// |-> also sort the list of articles by relevance, or other ways based on what newscatcher has
+//      // Criteria includes --- Title/keywords, Date, location, news source
+//      // |-> also sort the list of articles by relevance, or other ways based on what newscatcher has
 
-// convert Geo-Location to an actual usable location for newscatcher filtering
+
 
 function test() {
     var query = 'news'
@@ -28,6 +31,7 @@ function test() {
             console.log(data);
         })
 };
+
 
 function getLocation() {
     if (navigator.geolocation) {
