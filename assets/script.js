d0804b6a9ecb5347c074438526611b3f9148b17d
Blake H <blake.hollingsworth80@gmail.com>
2023-04-03 13:35:19 -0600

    added list of comments TODO

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,33 @@
-// function test() {
-//     var query = 'news'
+// On Website-Load we need to check for location and load local news articles
+// we need to set up a function for Jquery to add the news articles as a list to the page
 
-//     const url = "https://api.newscatcherapi.com/v2/search?" + new URLSearchParams({
-//         q: query,
-//         page_size: 20
-//     });
-//     fetch(url, {
-//         method: "GET",
-//         headers: {
-//             "x-api-key": "6XDeYVf8IA4XMhv7FFkL7LXdZvm61crB7KpC1zGTB8w"
-//         }
-//     })
-//         .then(function (response) {
-//             return response.json();
+// We need to have an event listeners for a button that will reload the news articles based on seach criteria typed by the user
+// Criteria includes --- Title/keywords, Date, location, news source
+// |-> also sort the list of articles by relevance, or other ways based on what newscatcher has
 
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//         })
-// };
+// convert Geo-Location to an actual usable location for newscatcher filtering
+
+function test() {
+    var query = 'news'
+
+    const url = "https://api.newscatcherapi.com/v2/search?" + new URLSearchParams({
+        q: query,
+        page_size: 20
+    });
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "x-api-key": "6XDeYVf8IA4XMhv7FFkL7LXdZvm61crB7KpC1zGTB8w"
+        }
+    })
+        .then(function (response) {
+            return response.json();
+
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+};
 
 function getLocation() {
     if (navigator.geolocation) {
